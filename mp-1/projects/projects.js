4d376f3040bcce53d0f1ed2982ef3dde59010d56
rypetrat <rypetrat@bu.edu>
2025-01-31 14:28:27 -0500

    began working on the javascript calculator

---
+++ mp-1/projects/projects.js
@@ -0,0 +1,9 @@
+function calculator() {
+    if(r<0) {
+        document.getElementById("output").style.color="red"
+    }
+    else {
+        document.getElementById("output").style.color="black"
+    }
+}
+
