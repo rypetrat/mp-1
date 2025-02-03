9dfb08a22827c0f22cc29f0787b5163e345ae215
rypetrat <rypetrat@bu.edu>
2025-02-03 12:59:35 -0500

    finished the javascript calculator, needs styling still

--- MPs/mp-1/projects/projects.js
+++
@@ -1,8 +0,0 @@
-
-
-if(r<0) {
-    document.getElementById("output").style.color="red"
-}
-else {
-    document.getElementById("output").style.color="black"
-}
--- mp-1/projects/projects.js
+++ mp-1/projects/projects.js
@@ -1,9 +1,90 @@
-function calculator() {
-    if(r<0) {
-        document.getElementById("output").style.color="red"
+// Make sure that if a number is negative it still always turns red and upon becoming positive again returns to black
+function calcAdd() {
+    let input1 = document.getElementById("num1-input").value
+    let input2 = document.getElementById("num2-input").value
+    let output = document.getElementById("retVal")
+
+    let result = Number(input1) + Number(input2)
+    if(result < 0) {
+        document.getElementById("retVal").style.color="red"
+        output.innerHTML = result
     }
     else {
-        document.getElementById("output").style.color="black"
+        document.getElementById("retVal").style.color="black"
+        output.innerHTML = result
     }
 }
 
+function calcSub() {
+    let input1 = document.getElementById("num1-input").value
+    let input2 = document.getElementById("num2-input").value
+    let output = document.getElementById("retVal")
+    
+    let result = Number(input1) - Number(input2)
+    if(result < 0) {
+        document.getElementById("retVal").style.color="red"
+        output.innerHTML = result
+    }
+    else {
+        document.getElementById("retVal").style.color="black"
+        output.innerHTML = result
+    }
+}
+
+function calcMult() {
+    let input1 = document.getElementById("num1-input").value
+    let input2 = document.getElementById("num2-input").value
+    let output = document.getElementById("retVal")
+
+    let result = Number(input1) * Number(input2)
+    if(result < 0) {
+        document.getElementById("retVal").style.color="red"
+        output.innerHTML = result
+    }
+    else {
+        document.getElementById("retVal").style.color="black"
+        output.innerHTML = result
+    }
+}
+
+function calcDiv() {
+    let input1 = document.getElementById("num1-input").value
+    let input2 = document.getElementById("num2-input").value
+    let output = document.getElementById("retVal")
+
+    let result = Number(input1) / Number(input2)
+    if(result < 0) {
+        document.getElementById("retVal").style.color="red"
+        output.innerHTML = result
+    }
+    else {
+        document.getElementById("retVal").style.color="black"
+        output.innerHTML = result
+    }
+}
+
+function calcPow() {
+    let input1 = document.getElementById("num1-input").value
+    let input2 = document.getElementById("num2-input").value
+    let output = document.getElementById("retVal")
+
+    let result = Number(input1)
+    for(let i = 1; i < input2; i++) {
+        result = result * input1
+    }
+
+    if(result < 0) {
+        document.getElementById("retVal").style.color="red"
+        output.innerHTML = result
+    }
+    else {
+        document.getElementById("retVal").style.color="black"
+        output.innerHTML = result
+    }
+}
+
+function calcClear() {
+    let input1 = document.getElementById("num1-input").value = ""
+    let input2 = document.getElementById("num2-input").value = ""
+    let output = document.getElementById("retVal").innerHTML = ""
+}
